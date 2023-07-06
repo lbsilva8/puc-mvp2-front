58c4d12470831d4781527c9f8cf7c67b39a33929
Lorena Borges <lorenaborgessilva@gmail.com>
2023-07-05 21:41:14 -0300

    pagina PlantasDetails

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Plantas from './pages/Plantas'
+import PlantasDetails from './pages/PlantasDetails'
 
 {/*import  Plantasdetails from './pages/Plantasdetails'*/}
 
@@ -13,7 +14,7 @@ export default function App() {
         <Routes> 
           <Route path='/' element={<Home />}></Route> 
           <Route path='/plantas' element={<Plantas />}></Route>
-          {/*<Route path='/plantas/:id' element={<Plantasdetails />}></Route>*/}
+          <Route path='/plantas/:id' element={<PlantasDetails />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/PlantasDetails.js
@@ -0,0 +1,23 @@
+import { useLocation, useNavigate, useParams } from "react-router"
+
+export default function PlantasDetails() {
+    let { state } = useLocation();
+    console.log(useLocation())
+    let { id } = useParams();   
+    const navigate = useNavigate()
+
+    return (
+        <div className="detalhes-plantas">
+          <div>
+            <button className="back" onClick={() => navigate('/plantas')}>Voltar</button>
+            <div>
+              <img className="detalhes-image" src={state.itens.image} alt="" />
+            </div>
+            <div>
+              <h2>{state.itens.nome} - {id}</h2>
+              <p>{state.itens.descrição}</p>
+            </div>
+          </div>
+        </div>
+      )
+}
