1aca14364a80bfb57dcb5550e4eb24653f2800c5
Lorena Borges <lorenaborgessilva@gmail.com>
2023-07-14 15:02:20 -0300

    ATUALIZAÇÕES FOOTER

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
-import {FaGithub} from 'react-icons/fa'
+import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
 
 export default function Footer() {
     return (
         <footer className='footer'>
-            <ul className='icones'>
                 <li>
-                    Desenvolvido por Lorena Borges <FaGithub></FaGithub>
+                    Desenvolvido por Lorena Borges 
+                    <a rel="linkedin" href="https://www.linkedin.com/in/lorenadasilvaborges/" target='_blank'><FaLinkedinIn></FaLinkedinIn></a>
+                    <a rel="Github" href="https://github.com/lbsilva8/" target='_blank'><FaGithub></FaGithub></a>
                 </li>
-            </ul>
         </footer>
     )
 }
