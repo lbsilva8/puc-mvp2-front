9f9e6dc28765e9095eb356a693161da4d6153b82
Lorena <lorenaborgessilva@gmail.com>
2024-06-28 02:17:19 -0300

    atualizando pagina detalhes da planta

--- src/pages/PlantasDetails.js
+++ src/pages/PlantasDetails.js
@@ -1,17 +1,14 @@
+/* Página com os detalhes da planta selecionada */
 import { useLocation, useNavigate, useParams } from 'react-router';
 
 export default function PlantasDetails() {
   let { state } = useLocation();
   console.log(useLocation());
   let { id } = useParams();
-  const navigate = useNavigate();
 
   return (
     <div className="detalhes-plantas">
       <div>
-        <button className="back" onClick={() => navigate('/plantas')}>
-          Voltar
-        </button>
         <div>
           <img className="detalhes-image" src={state.itens.image} alt="" />
         </div>
